377eb38cd310d543b05ab1dde0517f0f39f74bc1
Mihail Zafirovski <Mihail.Zafirovski@endava.com>
2022-07-03 16:14:13 +0200

    Added empty angular project with coresponding libraries

---
+++ fitness-army/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyABcr2L8w1Q0ObevvmlO43hQD43LcZXoY0",
+    authDomain: "fitness-army.firebaseapp.com",
+    databaseURL: "https://fitness-army-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fitness-army",
+    storageBucket: "fitness-army.appspot.com",
+    messagingSenderId: "232059287122",
+    appId: "1:232059287122:web:b4714c2ac949e82e73118b",
+    measurementId: "G-0WSZXDX4GR"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
