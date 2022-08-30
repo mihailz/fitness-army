c5a3e6a4f6783fe1c49667ed555ebd30b79a7e7d
mihailz <mihailzafirovski@gmail.com>
2022-08-30 11:38:38 +0200

    Added home landing page

--- fitness-army/src/app/modules/home-page/home-page.module.ts
+++ fitness-army/src/app/modules/home-page/home-page.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import {HomePageRoutingModule} from "./home-page-routing.module";
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 @NgModule({
   declarations: [
     HomePageComponent
   ],
-  imports: [
-    CommonModule,
-    HomePageRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        HomePageRoutingModule,
+        NzButtonModule
+    ]
 })
 export class HomePageModule { }
